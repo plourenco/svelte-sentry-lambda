346f15805dd8e2586939430ba480f899c08c99b3
plourenco <public@plourenco.com>
2024-06-05 01:19:00 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let name: string = 1; // error
+    let obj: {a: string, b: number} = {foo: 'bar'};
+
+    function greet(name: string) {
+        alert(`Hello, ${name}!`);
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
